a87018a064e6201728f24b3509362a4c43ee33e3
Sunghyun Hwang <me@sunghyunzz.com>
2018-08-16 21:16:37 +0900

    Add card data structures
    Signed-off-by: Sunghyun Hwang <me@sunghyunzz.com>

---
+++ holdem/core/cards.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from enum import IntEnum
+
+
+class Suit(IntEnum):
+    club = 1
+    heart = 2
+    diamond = 3
+    spade = 4
+
+
+class Rank(IntEnum):
+    ace = 1
+    two = 2
+    three = 3
+    four = 4
+    five = 5
+    six = 6
+    seven = 7
+    eight = 8
+    nine = 9
+    ten = 10
+    jack = 11
+    queen = 12
+    king = 13
+
+
+@dataclass(repr=False, frozen=True)
+class Card:
+    rank: Rank
+    suit: Suit
+
+    def __repr__(self):
+        return f'{self.rank.name} of {self.suit.name}s'
