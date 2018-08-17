4fa1131ccedced46c3fa0958b72a603de7f6f8df
devholic <me@devholic.io>
2018-08-17 15:35:10 +0900

    Add flush evaluation logic

--- holdem/core/madehands.py
+++ holdem/core/madehands.py
@@ -1,7 +1,15 @@
+from .cards import Card, Rank, Suit
 from enum import IntEnum
-from typing import Iterable, List
+from typing import List, Optional, Tuple
+from itertools import groupby
 
-from .cards import Card
+
+TRIPLE_LENGTH = 3
+FOUR_CARD_LENGTH = 4
+FLUSH_LENGTH_THRESHOLD = 5
+ROYALE_STRAIGHT_FLUSH_RANKS = [Rank.ace, Rank.ten, Rank.jack, Rank.queen, Rank.king]
+BACK_STRAIGHT_FLUSH_RANKS = [Rank.ace, Rank.two, Rank.three, Rank.four, Rank.five]
+MOUNTAIN_RANKS = ROYALE_STRAIGHT_FLUSH_RANKS
 
 
 class MadeHands(IntEnum):
@@ -10,44 +18,37 @@ class MadeHands(IntEnum):
     two_pair = 3
     triple = 4
     straight = 5
+    back_straight = 6
+    mountain = 7
     flush = 8
     full_house = 9
     four_card = 10
     straight_flush = 11
+    back_straight_flush = 12
+    royale_straight_flush = 13
 
 
-def _is_straight(ranks: Iterable[int]) -> bool:
-    sorted_ranks = sorted(ranks)
-    return all(y - x == 1 for x, y in zip(sorted_ranks, sorted_ranks[1:]))
+def evaluate(cards: List[Card]) -> Tuple[MadeHands, Rank, Suit]:
+    cards = cards[:]
+    cards.sort(key=lambda c: (c.suit, c.rank))
 
-
-def evaluate(cards: List[Card]) -> int:
-    suits = [c.suit for c in cards]
-
-    flush_table = {
-        min(suits.count(s), 5): _is_straight(c.rank for c in cards if c.suit == s)
-        for s in suits
+    suits = [list(v) for _, v in groupby(cards, key=lambda c: c.suit)]
+    suit_dict = {
+        len(suit) : suit for suit in suits
     }
-    flush = 5 in flush_table
+    longest_suit = suit_dict[max(suit_dict)]
 
-    ranks = [c.rank for c in cards]
-    pairs = [ranks.count(r) for r in set(ranks)]
-
-    if flush and flush_table[5]:
-        return MadeHands.straight_flush
-    elif 4 in pairs:
-        return MadeHands.four_card
-    elif 3 in pairs and 2 in pairs:
-        return MadeHands.full_house
-    elif flush:
-        return MadeHands.flush
-    elif _is_straight(c.rank for c in cards):
-        return MadeHands.straight
-    elif 3 in pairs:
-        return MadeHands.triple
-    elif pairs.count(2) >= 2:
-        return MadeHands.two_pair
-    elif 2 in pairs:
-        return MadeHands.one_pair
+    if (len(longest_suit) == FLUSH_LENGTH_THRESHOLD):
+        suit = longest_suit[0].suit
+        suits = list(map(lambda x: x.rank, longest_suit))
+        max_rank = longest_suit[4].rank
+        if (suits == ROYALE_STRAIGHT_FLUSH_RANKS):
+            return (MadeHands.royale_straight_flush, Rank.ace, suit)
+        elif (suits == BACK_STRAIGHT_FLUSH_RANKS):
+            return (MadeHands.back_straight_flush, Rank.ace, suit)
+        elif (all(y - x == 1 for x, y in zip(suits, suits[1:]))):
+            return (MadeHands.straight_flush, suit, max_rank)
+        else:
+            return (MadeHands.flush, suit, max_rank)
     else:
-        return MadeHands.top
+        pass # TODO
